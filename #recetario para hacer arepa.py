01d7a0dbca2b3c938b00a5e6c5f6a005d4d9905d
richard27027381 <168349350+richard27027381@users.noreply.github.com>
2024-04-28 14:49:30 -0400

    Add files via upload

---
+++ #recetario para hacer arepa.py
@@ -0,0 +1,8 @@
+#recetario para hacer arepa 
+agua = input("introduzca la taza de agua")
+harina = input("introduzca la taza de agua")
+sal = input("introduzca la cucharadita de sal 16/3")
+aceite = input("introduzca cucharadita de aceite 16/3")
+bol = int(agua) + int(harina) + int(sal)
+budare = int(aceite) + int(bol)   
+print(f"el valor total de esta receta es {budare}")
